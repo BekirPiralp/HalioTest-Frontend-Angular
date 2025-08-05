ca866605f6ddd268292e22963e3fd2042485d4e6
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-05 13:28:38 +0300

    form kontrol temel yapısı oluşturuldu ve oluşturuluyor...

--- src/components/page/register/main/register/register.ts
+++ src/components/page/register/main/register/register.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    FormsModule,CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  
+onSubmit() {
+throw new Error('Method not implemented.');
+}
 
 }
