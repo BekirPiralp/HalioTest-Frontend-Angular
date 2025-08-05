e0ea83ebef49808df93aec9126c5f2d0cee7c2ca
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-05 04:15:00 +0300

    getuser oluşturuldu

--- src/services/main/users.service.ts
+++ src/services/main/users.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BaseUrl } from '../url/base-url';
 import { BaseService } from './base/base.service';
 import { UsersModel } from '../../models/concrete/entity-models/users.model';
 import { UsersUrl } from '../url/entity-models-url/users.url';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,16 @@ export class UsersService extends BaseService<UsersModel, UsersUrl> {
   constructor(http: HttpClient) {
     super(http, new UsersUrl());
 
+  }
+
+  getUser(email:string):Observable<UsersModel>{
+    const param = new HttpParams().set('email',email);
+
+    return this._http.post<UsersModel>(this._url._getUser(),null,{params:param}).pipe(
+      catchError((error:Error)=>{
+        console.error(`[${this.constructor.name}]: ${error.message}`);
+        return throwError(()=>error);
+      })
+    );
   }
 }
