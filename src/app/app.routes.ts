ede627af1d90be1cf808aefb3009fb59d93c2c2a
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-05 11:03:45 +0300

    register routa eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from '../components/page/login/main/login/login';
+import { Register } from '../components/page/register/main/register/register';
 
 export const routes: Routes = [
     {path:'login',component:Login},
+    {path:'register',component:Register},
     {path:"**",redirectTo:"login"}
 ];
