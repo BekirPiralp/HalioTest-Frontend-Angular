09f87fc1208765e9f98c7cb0870b9950a04b5213
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-05 06:35:01 +0300

    test için hazı login kullanıldı ve methodlar yazıldı

--- src/components/page/login/main/login/login.ts
+++ src/components/page/login/main/login/login.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../../../../services/login.service';
+import { UsersService } from '../../../../../services/main/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private login:LoginService,
+    private userService:UsersService,
+    private router:Router) {
+    
+  }
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    if (this.email && this.password) {
+      this.login.Login(this.email,this.password).subscribe(response=>{
+        if(response)
+        {
+          this.userService.getUser(this.email).subscribe(
+            response=>{
+              localStorage.clear();
+              localStorage.setItem("user",JSON.stringify(response));
+              this.router.navigate(["/"]);
+            }
+          )
+        }else{
+          //alertify gelecek
+          alert("Giriş başarısız");
+        }
+      } 
+      )
+    }
+  }
 }
