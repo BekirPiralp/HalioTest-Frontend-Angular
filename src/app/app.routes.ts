667736e278abe4f838b142557ead56422059670e
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-08 02:34:23 +0300

    routa şimdilik home eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from '../components/page/login/main/login/login';
 import { Register } from '../components/page/register/main/register/register';
+import { Home } from '../components/page/home/main/home/home';
 
 export const routes: Routes = [
+    {path:"home",component:Home},
+    {path:"",component:Home},
+    {path:"#",component:Home},
     {path:'login',component:Login},
     {path:'register',component:Register},
     {path:"**",redirectTo:"login"}
