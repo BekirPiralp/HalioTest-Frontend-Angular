dff2660794015cd9f0e524bad0f047fe3934818b
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-08 04:17:25 +0300

    navbar oluşturuldu

--- src/components/page/home/other/navbar/navbar.ts
+++ src/components/page/home/other/navbar/navbar.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersModel } from '../../../../../models/concrete/entity-models/users.model';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-home-navbar',
   imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit {
 
+  constructor( private router: Router) {
+
+  }
+
+  user= new UsersModel(0,'Foo','Fouter','5554443322','test01@mail.com','asdasdas');
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user');
+    
+    if(storedUser)
+      this.user = JSON.parse(storedUser) as UsersModel;
+    else
+      this.exit();
+  
+  }
+
+  
+
+  exit():void{
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
 }
