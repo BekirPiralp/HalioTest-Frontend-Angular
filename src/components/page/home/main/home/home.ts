40eab5b59188709342c00576b0b3326761a5b05a
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-08 04:16:55 +0300

    .

--- src/components/page/home/main/home/home.ts
+++ src/components/page/home/main/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../other/navbar/navbar";
+import { Sidebar } from "../../other/sidebar/sidebar";
+import { CaseFilter } from "../../other/case-filter/case-filter";
+import { CaseList } from "../../other/case-list/case-list";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, Sidebar, CaseFilter, CaseList],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
 }
