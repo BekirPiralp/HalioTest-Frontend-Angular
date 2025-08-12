88e6c6ca13ddd2b7841df64325df17d4633f373a
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-12 07:09:54 +0300

    tool combo ayarlanıyor

--- src/components/page/home/other/case-filter/case-filter.ts
+++ src/components/page/home/other/case-filter/case-filter.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Params } from '../../../../tools/combo-box/models/abstract/params';
+import { FilterModel } from '../../../../tools/combo-box/models/concrete/filter-model';
+import { ComboBox } from "../../../../tools/combo-box/combo-box";
 
 @Component({
   selector: 'app-home-case-filter',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ComboBox],
   templateUrl: './case-filter.html',
   styleUrl: './case-filter.css'
 })
 export class CaseFilter {
-  _selectedKey:string|undefined;
-  _filterComboValue: FilterModel | undefined;
-  _filterListDefault: Array<FilterModel> = [
+  _filterList: Array<FilterModel> = [
     /*"@case-name:",
     "@case-description:",
     "@status:",
@@ -20,6 +21,7 @@ export class CaseFilter {
     new FilterModel(
       {
         key:"@case-name:",
+        isDefault: true,
         func:(a)=>{
           console.log("function içi:")
           console.log(a)
@@ -43,87 +45,22 @@ export class CaseFilter {
     ),
     new FilterModel(
       <Params>{
-        key:"@case-description:"
+        key:"@case-description:",
+        isDefault: true,
       }),
     new FilterModel({
-      key:"@status:"
+      key:"@status:",
+      isDefault: true,
     }),
     new FilterModel({
-      key:"@status-description:"
+      key:"@status-description:",
+      isDefault: true,
     }),
     new FilterModel({
-      key:"@asigned-user:"
-    }),
-    new FilterModel({
-      key:"",
-      func:()=>{
-        this.listClear();
-      }
+      key:"@asigned-user:",
+      isDefault: true,
     })
   ]
-  _filterList = [...this._filterListDefault];
-
-  filterValueChange() {
-    let selectedValue= this._filterList.find(p=>p.key === this._selectedKey);
-    
-    if(!selectedValue) // for text copy paste
-      selectedValue = this._filterList.find(p=> p.key &&this._selectedKey?.startsWith(p.key))
-    
-    if(selectedValue){
-      console.log(selectedValue)
-      console.log(this._filterList)
-      if(selectedValue.func){
-        selectedValue.func(selectedValue);
-      }
-    }else{
-      console.log("girdi")
-      if(this._filterList.length != this._filterListDefault.length)
-      {
-        this.listClear();
-      }
-    }
-    console.log("değişimlist")
-    console.log(this._filterList)
-  }
 
 
-  onFilterEnter() {
-    this.filterValueChange(); //kopyalanan değere göre veri geldi ve liste de ise ikincil tekrar kontrol
-  }
-
-
-  listClear(){
-    this._filterList.length=0;
-    this._filterList.push(...this._filterListDefault);
-  }
 }
-
-class FilterModel {
-  constructor(
-              params?: Params | undefined 
-              ) {
-
-    if (params) {
-      this.key = params.key;
-      this.data = params.data;
-      this.func = params.func;
-    }
-  }
-  isdefault:boolean |undefined;
-  key: string | undefined;
-  data: object | undefined;
-  func: ((filterModel?: FilterModel) => void) | undefined;
-  protected filterModel = this;
-
-  get value():string|undefined{
-   return this.key?.match("@[^:]+:\s*(?<value>.*)")?.groups?.['value'] || undefined;
-  }
-
-  toString():string{
-    return this.key ||""
-  }
-}
-
-type Params = { key?: string,
-                        data?: object, 
-                        func?: ((filterModel?: FilterModel) => void )}
--- src/components/tools/combo-box/combo-box.ts
+++ src/components/tools/combo-box/combo-box.ts
@@ -1,15 +1,73 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { IFilterModel } from './models/abstract/ifilter-model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FILTER_HANDLER } from './models/tokens/filter-handler.token';
+import { IFilterHandler } from './models/abstract/ifilter-handler';
 
 @Component({
-  selector: 'app-combo-box',
-  imports: [],
+  selector: 'app-tool-combo-box',
+  imports: [CommonModule,FormsModule],
   templateUrl: './combo-box.html',
   styleUrl: './combo-box.css'
 })
 export class ComboBox {
+
+  constructor(@Inject(FILTER_HANDLER) private _filterHandler:IFilterHandler) {
+    
+  }
   
   @Input() filterList?:IFilterModel[];
-  @Output() filterListChange = this.filterList;
-  @Output() selectedValue?:IFilterModel;
+  @Output() filterListChange = new EventEmitter<IFilterModel[]|undefined>();
+  @Output() selectedValueChange = new EventEmitter<IFilterModel|undefined>();
+
+  @Input()  set selectedKey(val:string|undefined){
+    this._selectedKey = val;
+    setTimeout(()=>{
+      this.filterHandle();
+    })
+  }
+  @Output() selectedKeyChange = new EventEmitter<string|undefined>();
+
+
+  _selectedKey: string|undefined;
+
+
+  
+
+
+  filterValueChange() {
+    this.selectedKeyChange.emit(this._selectedKey);
+
+    this.filterHandle();
+  }
+
+  filterHandle(){
+    let _selectedValue = this.filterList?.find(p=>p.key === this._selectedKey);
+    
+    if(!_selectedValue) // for text copy paste
+      _selectedValue = this.filterList?.find(p=> p.key &&this._selectedKey?.startsWith(p.key))
+    
+    this.selectedValueChange.emit(_selectedValue);
+    
+    if(_selectedValue){
+      this._filterHandler.handle(_selectedValue);
+    
+    }else{
+      this.listClear();
+    }
+  }
+
+
+  onFilterEnter() {
+    this.filterHandle(); //kopyalanan değere göre veri geldi ve liste de ise ikincil tekrar kontrol
+  }
+
+
+  listClear(){
+    if(this.filterList && this.filterList.some(p=>p.isDefault !==true)){
+      this.filterList=this.filterList.filter(p=>p.isDefault);
+      this.filterListChange.emit(this.filterList);
+    }
+  }
 }
