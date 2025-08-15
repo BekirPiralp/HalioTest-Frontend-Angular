019b1801e22cad6d2eeb5262bcc2fc6a0c4c2b9e
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-15 16:42:41 +0300

    case filter yazılıyor

--- src/components/page/home/other/case-filter/case-filter.ts
+++ src/components/page/home/other/case-filter/case-filter.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { Params } from '../../../../tools/combo-box/models/abstract/params';
 import { FilterModel } from '../../../../tools/combo-box/models/concrete/filter-model';
 import { ComboBox } from "../../../../tools/combo-box/combo-box";
+import { CasesService } from '../../../../../services/main/cases.service';
+import { CaseStatusService } from '../../../../../services/main/case-status.service';
+import { CaseStatus } from '../../../../../models/concrete/other/case-status';
 
 @Component({
   selector: 'app-home-case-filter',
@@ -12,6 +15,10 @@ import { ComboBox } from "../../../../tools/combo-box/combo-box";
   styleUrl: './case-filter.css'
 })
 export class CaseFilter {
+  constructor(private _caseService:CasesService,private _caseStatusService:CaseStatusService
+  ) {
+    
+  }
   _filterList: Array<FilterModel> = [
     /*"@case-name:",
     "@case-description:",
@@ -22,24 +29,20 @@ export class CaseFilter {
       {
         key:"@case-name:",
         isDefault: true,
-        func:(a)=>{
-          console.log("function içi:")
-          console.log(a)
-          console.log(a?.toString());
-          console.log("bitti");
-          if(!this._filterList.find(p=>p.key==="@case-name:asd"))
-              this._filterList.push(...[
-                new FilterModel(
-                    {
-                      key:"@case-name:asd",
-                      func:(a)=>{
-                        console.log("function içi:")
-                        console.log(a)
-                        console.log(a?.value);
-                        console.log("bitti");
-                      }
-                    })
-              ]);
+        func:(_fModel)=>{
+          this._caseService.getAll().subscribe(response=>{
+            if(response?.length>0){
+              response.forEach((item)=>{
+                this._filterList.push(new FilterModel({
+                  key:`@case-name:${item.name}`,
+                  data: item,
+                  func(filterModel) {
+                    // case işlem; ve case list güncellencek
+                  },
+                }))
+              })
+            }
+          })
         }
       }
     ),
@@ -47,10 +50,32 @@ export class CaseFilter {
       <Params>{
         key:"@case-description:",
         isDefault: true,
+        func:(filterModel)=>{
+          let searchKey = filterModel?.value;
+          if(searchKey?.trim()){
+            this._caseService.getBySearchtoDescription(searchKey).subscribe(reponse=>{
+              //case list güncellenecek
+            })
+          }
+        },
       }),
     new FilterModel({
       key:"@status:",
       isDefault: true,
+      func:(filterModel)=>{
+        //enum a göre yeni sattusler ile vryant eklenecek
+         Object.values(CaseStatus).filter((v):v is number=> typeof v === 'number').forEach((item)=>{
+          console.log(item)
+          this._filterList.push(new FilterModel({
+            key:`${filterModel?.key}${CaseStatus[item as number]}`,
+            func:(filterModel)=>{
+              this._caseStatusService.getAll().subscribe(reponse=>{
+                //reponse.filter(p=>p.status && p.status === item)
+              })  
+            },
+          }));
+         });
+      },
     }),
     new FilterModel({
       key:"@status-description:",
