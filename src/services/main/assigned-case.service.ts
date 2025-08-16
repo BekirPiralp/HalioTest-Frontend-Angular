a0a8b289d30e9a85abd3d0cf85ac9a9e3c02f110
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-16 15:52:29 +0300

    user id ye göre assigned listesi çağrıma servise eklendi

--- src/services/main/assigned-case.service.ts
+++ src/services/main/assigned-case.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { BaseService } from './base/base.service';
 import { AssignedCaseModel } from '../../models/concrete/entity-models/assigned-case.model';
 import { AssignedCaseUrl } from '../url/entity-models-url/assigned-case.url';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +13,16 @@ export class AssignedCaseService extends BaseService<AssignedCaseModel,AssignedC
     super(http,new AssignedCaseUrl());
   }
   
+  getByUserId(id:number):Observable<AssignedCaseModel[]>{
+    const params = new HttpParams().set("searchKey",`${id}`)
+    .set("orderField","id").set("desc",false);
+    
+    return this._http.post<AssignedCaseModel[]>(this._url._getByFilterByOrderByRemoved(),
+    ["caseId","cases","description","id","user"],{params:params}).pipe(
+      catchError((error:Error)=>{
+        console.error(`[${this.constructor.name}]: ${error.message}`);
+        return throwError(()=>error);
+      })
+    )
+  }
 }
