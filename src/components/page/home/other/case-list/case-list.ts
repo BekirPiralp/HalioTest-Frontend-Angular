9404e7059bd15c6a31dd2926ae6bdcf73b8dabd0
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-18 09:46:19 +0300

    Case list temelleri oluşturuldu

--- src/components/page/home/other/case-list/case-list.ts
+++ src/components/page/home/other/case-list/case-list.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { CasesModel } from '../../../../../models/concrete/entity-models/cases.model';
+import { CaseCard } from "../../../../tools/case-card/case-card";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-case-list',
-  imports: [],
+  imports: [CommonModule,CaseCard],
   templateUrl: './case-list.html',
   styleUrl: './case-list.css'
 })
 export class CaseList {
 
+  @Input() listCase:CasesModel[]|undefined = Array<CasesModel>(10); //[new CasesModel(1,"testCase","test desc",new Date(),new Date(),1,undefined)];
+  protected _listCase:CasesModel[]|undefined = Array<CasesModel>(100);;
 }
