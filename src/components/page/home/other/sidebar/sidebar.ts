fce35a77ce1900a7cedc3f2f1c160151a823d326
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-19 16:30:51 +0300

    btn klick ile filtreleme ayarlanıyor

--- src/components/page/home/other/sidebar/sidebar.ts
+++ src/components/page/home/other/sidebar/sidebar.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DefaultFilterKeys } from '../../../../../models/concrete/other/default-filter-keys';
+import { UsersModel } from '../../../../../models/concrete/entity-models/users.model';
 
 @Component({
   selector: 'app-home-sidebar',
@@ -7,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
+  
+  constructor(){
+    const storedUser = localStorage.getItem('user');
+        
+        if(storedUser)
+          this.user = JSON.parse(storedUser) as UsersModel;  
+  }
+  
+  protected fKeys=DefaultFilterKeys;
+
+  @Output() filterKey= new EventEmitter<string>();
+
+  protected user:UsersModel|undefined;
+
+  protected filterKeySet(arg0: DefaultFilterKeys) {
+    switch(arg0){
+      case DefaultFilterKeys.openedUser:
+      case DefaultFilterKeys.asignedUser:
+        if(this.user)
+        this.filterKey.emit(arg0+this.user.name+"-"+this.user.surName);
+        break;
+      case DefaultFilterKeys.default:
+      default:
+        this.filterKey.emit(DefaultFilterKeys.default)
+        break;
+    }
+  }
 
 }
