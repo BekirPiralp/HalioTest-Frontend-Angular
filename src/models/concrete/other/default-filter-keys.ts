745d48681388b8cb9428a0f860d1caf40f93bc1c
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-19 16:24:51 +0300

    Dafeult filter keys tanımlaması yapıldı

---
+++ src/models/concrete/other/default-filter-keys.ts
@@ -0,0 +1,9 @@
+export enum DefaultFilterKeys {
+    caseName="@case-name:",
+    caseDescription="@case-description:",
+    status="@status:",
+    statusDescription="@status-description:",
+    asignedUser="@asigned-user:",
+    openedUser="@opened-user:",
+    default=""
+}
