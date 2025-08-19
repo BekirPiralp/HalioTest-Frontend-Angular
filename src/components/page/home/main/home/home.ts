d2dbab072fbd5ed6c150e886eb4ccf1e1beb83b3
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-19 10:29:31 +0300

    hopage de oluşum esnası listcase eklenmesi sağlandı

--- src/components/page/home/main/home/home.ts
+++ src/components/page/home/main/home/home.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navbar } from "../../other/navbar/navbar";
 import { Sidebar } from "../../other/sidebar/sidebar";
 import { CaseFilter } from "../../other/case-filter/case-filter";
 import { CaseList } from "../../other/case-list/case-list";
 import { CommonModule } from '@angular/common';
 import { CasesModel } from '../../../../../models/concrete/entity-models/cases.model';
+import { CasesService } from '../../../../../services/main/cases.service';
 
 @Component({
   selector: 'app-home',
@@ -13,5 +14,18 @@ import { CasesModel } from '../../../../../models/concrete/entity-models/cases.m
   styleUrl: './home.css'
 })
 export class Home {
-  public listCases:CasesModel[]|undefined;
+  constructor(private _casesService:CasesService) {
+    this._casesService.getAllDesc().subscribe((result)=>{
+      this._listCases=result;
+    })
+  }
+  
+  private _listCases:CasesModel[]|undefined;
+  public set listCases(val:CasesModel[]|undefined){
+    this._listCases = val;
+    console.log(this._listCases)
+  }
+  public get listCases(){
+    return this._listCases;
+  }
 }
