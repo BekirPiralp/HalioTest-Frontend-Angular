150e2d013bc0ade6484a349a9f71f7a3d2326775
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-21 19:05:41 +0300

    case form yazıldı, fakat form yapıtp template den reactive e çekilecek

--- src/components/tools/case-form/case-form.ts
+++ src/components/tools/case-form/case-form.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CaseFormModalService } from './services/case-form-modal.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CasesModel } from '../../../models/concrete/entity-models/cases.model';
 
 @Component({
-  selector: 'app-case-form',
-  imports: [],
+  selector: 'app-tool-case-form',
+  imports: [FormsModule,CommonModule],
   templateUrl: './case-form.html',
   styleUrl: './case-form.css'
 })
 export class CaseForm {
+onSubmit($event: Event) {
+  alert(this.caseForCretate);
+  console.log(this.caseForCretate)
+}
+
+  constructor(private thisModalService: CaseFormModalService) {
+    this.subscribeToModalService()
+  }
+
+  protected isOpen = false;
+
+  protected caseForCretate:CasesModel = CasesModel.prototype;
+
+
+  subscribeToModalService() {
+    this.thisModalService.isOpen$.subscribe((result) => {
+      this.isOpen = result;
+      console.log(this.caseForCretate)
+    });
+  }
+
+  close() {
+    console.log("girdi")
+    this.thisModalService.close();
+  }
 
 }
