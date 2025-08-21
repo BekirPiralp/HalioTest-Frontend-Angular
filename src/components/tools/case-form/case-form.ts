49edbf990ab7098af522025e95c7407d72071429
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-21 12:10:55 +0300

    case ekelemk için forum oluşturuluyor

---
+++ src/components/tools/case-form/case-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-form',
+  imports: [],
+  templateUrl: './case-form.html',
+  styleUrl: './case-form.css'
+})
+export class CaseForm {
+
+}
