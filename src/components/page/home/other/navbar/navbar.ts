2cfb74ae4b4631c842482a63c62a6df42bc43169
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-22 12:45:40 +0300

    nav bar içi logined user getirme işlemi service ile sağlandı

--- src/components/page/home/other/navbar/navbar.ts
+++ src/components/page/home/other/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersModel } from '../../../../../models/concrete/entity-models/users.model';
 import { Router } from '@angular/router';
+import { LocalStorageService } from '../../../../../services/other/local-storage.service';
 
 @Component({
   selector: 'app-home-navbar',
@@ -10,19 +11,20 @@ import { Router } from '@angular/router';
 })
 export class Navbar implements OnInit {
 
-  constructor( private router: Router) {
-
+  constructor( private router: Router,_localStorageService:LocalStorageService) {
+    if(_localStorageService.loginedUser)
+      this.user = _localStorageService.loginedUser;
   }
 
   user= new UsersModel(0,'Foo','Fouter','5554443322','test01@mail.com','asdasdas');
 
   ngOnInit(): void {
-    const storedUser = localStorage.getItem('user');
+    // const storedUser = localStorage.getItem('user');
     
-    if(storedUser)
-      this.user = JSON.parse(storedUser) as UsersModel;
-    else
-      this.exit();
+    // if(storedUser)
+    //   this.user = JSON.parse(storedUser) as UsersModel;
+    // else
+    //   this.exit();
   
   }
 
