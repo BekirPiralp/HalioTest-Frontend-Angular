cc8df446eadaa243fe47e136b766607cf68258cf
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-24 19:41:52 +0300

    Assigned işlemi için olan modal service hompage teki yerine yerleştirildi

--- src/components/page/home/main/home/home.ts
+++ src/components/page/home/main/home/home.ts
@@ -8,10 +8,11 @@ import { CasesModel } from '../../../../../models/concrete/entity-models/cases.m
 import { CasesService } from '../../../../../services/main/cases.service';
 import { ListCaseService } from '../../other/case-filter/services/list-case.service';
 import { CaseForm } from "../../../../tools/case-form/case-form";
+import { AssignedAssingCard } from "../../../../tools/assigned-assing-card/assigned-assing-card";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Sidebar, CaseFilter, CaseList, CaseForm],
+  imports: [Navbar, Sidebar, CaseFilter, CaseList, CaseForm, AssignedAssingCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
