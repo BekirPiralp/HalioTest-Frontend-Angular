e5a53e6b3c8fa1b98e65c535e7099a3dccfc3369
Bekir Piralp <bekir01piralp@gmail.com>
2025-08-25 05:53:07 +0300

    assigned ve opened filterda ilgili caseler intilize edilirken ufakçaplı geçikme var

--- src/components/page/home/other/case-filter/models/assigned-user-filter-model.ts
+++ src/components/page/home/other/case-filter/models/assigned-user-filter-model.ts
@@ -1,4 +1,4 @@
-import { tap, of, forkJoin, map } from "rxjs";
+import { tap, of, forkJoin, map, Observable, concatMap, firstValueFrom } from "rxjs";
 import { CasesModel } from "../../../../../../models/concrete/entity-models/cases.model";
 import { UsersModel } from "../../../../../../models/concrete/entity-models/users.model";
 import { DefaultFilterKeys } from "../../../../../../models/concrete/other/default-filter-keys";
@@ -9,6 +9,7 @@ import { ListCaseService } from "../services/list-case.service";
 import { UsersService } from "../../../../../../services/main/users.service";
 import { FilterListService } from "../../../../../tools/combo-box/services/filter-list.service";
 import { AssignedCaseService } from "../../../../../../services/main/assigned-case.service";
+import { SpinnerService } from "../../../../../tools/spinner/service/spinner.service";
 
 export class AssignedUserFilterModel extends FilterModel implements IFilterModel {
   constructor(
@@ -16,7 +17,8 @@ export class AssignedUserFilterModel extends FilterModel implements IFilterModel
     private _filterListService: FilterListService,
     private _caseService: CasesService,
     private _usersService: UsersService,
-    private _assignedCaseService: AssignedCaseService
+    private _assignedCaseService: AssignedCaseService,
+    private _spinnerService:SpinnerService
   ) {
     super({
       key: DefaultFilterKeys.asignedUser,
@@ -28,22 +30,34 @@ export class AssignedUserFilterModel extends FilterModel implements IFilterModel
 
   }
 
-  private isRunProcess=false;
+  private isRunProcess = false;
+  private _filterList: IFilterModel[] | undefined;
 
   private AssignedUserFilterFunc(filterModel?: IFilterModel, secondaryProccess?: () => void) {
 
+    this._spinnerService.spinnerDisplay$=true;
 
     //Users listesini getir
-    let forRunProcess=this._usersService.getAll().pipe(map((response) => {
+    this._usersService.getAll().pipe(map(async (response) => {
+
       if (response && response.length > 0) {
-        let _filterList = this._filterListService.filterListInstant;
-        let kntrlList = _filterList?.filter(p => p.data).map(p => p.data as UsersModel)
+
+        this._filterList = this._filterListService.filterListInstant;
+        
+        if(!this._filterList)
+          this._filterList = await firstValueFrom(this._filterListService.filterList$);
+
+        // let _filterList = this._filterListService.filterListInstant;
+        let kntrlList = this._filterList?.filter(p => p.data && p.key!.toLowerCase().startsWith(DefaultFilterKeys.asignedUser.toLowerCase())).map(p => p.data as UsersModel)
         //userları filter list e ekle 
         response.forEach((item) => {
           //userların daha önce eklenmemiş olduğunu kanıtla
-          if (!kntrlList?.some(p => p.id === item.id)){
+          if (!kntrlList?.some(p => p.id === item.id)) {
+            if (!this._filterList)
+              this._filterList = [];
+
             //users filter liste ekemek için model oluşumu
-            _filterList?.push(new FilterModel({
+            this._filterList?.push(new FilterModel({
               key: `${filterModel?.key}${item.name}-${item.surName}`,
               data: item,
               func: (itemModel) => {
@@ -83,24 +97,23 @@ export class AssignedUserFilterModel extends FilterModel implements IFilterModel
                 )
               }
             }));
-            this.isRunProcess=true;
+
+            console.log("burda")
+            this.isRunProcess = true;
           }
         });
 
-        this._filterListService.filterList$ = _filterList;
-      }
-    }));
+        this._filterListService.filterList$ = this._filterList;
+        if (!this.isRunProcess)
+          return;
 
-    forkJoin([forRunProcess]).subscribe(()=>{
-      if(!this.isRunProcess)
-        return;
-      
-      this.isRunProcess=false;
-      
-      if(secondaryProccess){
-        secondaryProccess();
+        this.isRunProcess = false;
+
+        if (secondaryProccess)
+          secondaryProccess();
+
       }
-    })
+    })).subscribe(()=>this._spinnerService.spinnerDisplay$=false);
 
   }
 }
--- src/components/page/home/other/case-filter/models/opened-user-filter-model.ts
+++ src/components/page/home/other/case-filter/models/opened-user-filter-model.ts
@@ -1,4 +1,4 @@
-import { forkJoin, map, Observable } from "rxjs";
+import { concatMap, EMPTY, firstValueFrom, forkJoin, map, Observable, take } from "rxjs";
 import { UsersModel } from "../../../../../../models/concrete/entity-models/users.model";
 import { DefaultFilterKeys } from "../../../../../../models/concrete/other/default-filter-keys";
 import { CasesService } from "../../../../../../services/main/cases.service";
@@ -7,13 +7,17 @@ import { IFilterModel } from "../../../../../tools/combo-box/models/abstract/ifi
 import { FilterModel } from "../../../../../tools/combo-box/models/concrete/filter-model";
 import { FilterListService } from "../../../../../tools/combo-box/services/filter-list.service";
 import { ListCaseService } from "../services/list-case.service";
+import { SpinnerService } from "../../../../../tools/spinner/service/spinner.service";
+import alertify from "alertifyjs";
 
 export class OpenedUserFilterModel extends FilterModel implements IFilterModel {
   constructor(
     private _listCaseService: ListCaseService,
     private _filterListService: FilterListService,
     private _caseService: CasesService,
-    private _usersService: UsersService) {
+    private _usersService: UsersService,
+    private _spinnerService:SpinnerService
+  ) {
     super({
       key: DefaultFilterKeys.openedUser,
       isDefault: true,
@@ -22,35 +26,51 @@ export class OpenedUserFilterModel extends FilterModel implements IFilterModel {
       },
     });
 
+
   }
 
-  private isRunProcess = false;
+  private isRunProcess = false; //secondaryprocres için
+  private _filterList: IFilterModel[] | undefined;
+
+  set filterList(value:IFilterModel[] | undefined){
+    this._filterList=value
+  }
 
   private OpenedUserFilterFunc(filterModel?: IFilterModel, secondaryProccess?: () => void) {
     //Users listesini getir
-    let forRunProcess = this._usersService.getAll().pipe(map((response) => {
+    this._spinnerService.spinnerDisplay$=true;
+    this._usersService.getAll().pipe(map(async (response) => {
+      
       if (response && response.length > 0) {
-
-        let _filterList = this._filterListService.filterListInstant;
-
-        let kntrlList = _filterList?.filter(p => p.data).map(p => p.data as UsersModel)
+        
+        this._filterList = this._filterListService.filterListInstant;
+        
+        if(!this._filterList)
+          this._filterList = await firstValueFrom(this._filterListService.filterList$);
+        
+        let kntrlList = this._filterList?.filter(p => p.data && p.key!.toLowerCase().startsWith(DefaultFilterKeys.openedUser.toLowerCase())).map(p => p.data as UsersModel)
+        
         //userları filter list e ekle 
         response.forEach((item) => {
           //userların daha önce eklenmemiş olduğunu kanıtla
-          if (!kntrlList?.some(p => p.id === item.id)) {
+          if (!kntrlList || !kntrlList?.some(p => p.id === item.id)) {
+            
+            if (!this._filterList)
+              this._filterList = [];
+            
             //users filter liste ekemek için model oluşumu
-            _filterList?.push(new FilterModel({
+            this._filterList?.push(new FilterModel({
               key: `${filterModel?.key}${item.name}-${item.surName}`,
               data: item,
               func: (itemModel) => {
                 let user = <UsersModel>(itemModel?.data);
 
                 //user a ait listeyi getir
-                if (user)
+                if (user){
                   this._caseService.getByUserId(user.id).subscribe(result => {
 
                     this._listCaseService.listCase$ = result;
-                  })
+                  })}
                 else {
                   this._listCaseService.listCase$ = undefined;
                 }
@@ -58,24 +78,24 @@ export class OpenedUserFilterModel extends FilterModel implements IFilterModel {
               }
             }));
 
-            this.isRunProcess = true;
+            this.isRunProcess = true; //yeni user var ise
           }
         });
-        console.log("girdi55")
-        console.log(_filterList)
-        this._filterListService.filterList$ = _filterList;
+
+        this._filterListService.filterList$ = this._filterList;
+
+        if (!this.isRunProcess)
+          return;
+
+        this.isRunProcess = false;
+
+        if (secondaryProccess)
+          secondaryProccess();
+
       }
-    }));
+    })).subscribe(()=>{
+      this._spinnerService.spinnerDisplay$=false;
+    });
 
-    forkJoin([forRunProcess]).subscribe(() => {
-      if (!this.isRunProcess)
-        return;
-
-      this.isRunProcess = false;
-
-      if (secondaryProccess) {
-        secondaryProccess();
-      }
-    })
   }
 }
